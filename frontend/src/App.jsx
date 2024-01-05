7c5f82963a780c053558741f12b801ca5b14df6d
SeongEun <bongfam2@naver.com>
2024-01-05 18:09:24 +0900

    리액트 로그인페이지

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,40 @@
 /* eslint-disable prettier/prettier */
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
+import NOTI from './asset/KakaoTalk_20240105_025742662.png';
+import NAVER from './asset/NAVER.png';
+import GOOGLE from './asset/GOOGLE.png';
+import KAKAO from './asset/KAKAO.png';
 
 const MainDiv = styled.div`
   display: flex;
+  flex-direction: column;
+  align-items: center;
   width: 100%;
   justify-content: center;
+  background-color: #F9F9F9;
+  height: 100vh;
+  display: flex;
 `;
 
+const Mainlogo = styled.img`
+  width: 10rem;
+  height: 13rem;
+  padding: 1.2rem;
+`;
+
+const SocialImg = styled.img`
+  width: 3.5rem;
+  height: 3.5rem;
+  border-radius: 3rem;
+`;
+const SocialWrap = styled.div`
+  flex-direction: row;
+`
+const LoginImage = styled.image`
+  width: 30px;
+  height: 30px;
+`
 function App() {
   const [message, setMassege] = useState([]);
 
@@ -24,7 +51,13 @@ function App() {
   return (
     <>
       <MainDiv>
+        <Mainlogo src={NOTI} />
         <h1>{message}</h1>
+        <SocialWrap>
+          <SocialImg style={{ margin: '1rem' }} src={GOOGLE} />
+          <SocialImg style={{ margin: '1rem' }} src={NAVER} />
+          <SocialImg style={{ margin: '1rem' }} src={KAKAO} />
+        </SocialWrap>
       </MainDiv>
     </>
   );
