b921393651d87c63ed7bfad23757a48edcb4286f
SeongEun <bongfam2@naver.com>
2024-01-08 18:07:06 +0900

    경로 설정

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,10 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable prettier/prettier */
 import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import styled from "styled-components";
-import NOTI from './asset/KakaoTalk_20240105_025742662.png';
-import NAVER from './asset/NAVER.png';
-import GOOGLE from './asset/GOOGLE.png';
-import KAKAO from './asset/KAKAO.png';
+import Login from "./pages/Login";
+import Main from "./pages/Main";
 
 const MainDiv = styled.div`
   display: flex;
@@ -18,48 +17,15 @@ const MainDiv = styled.div`
   display: flex;
 `;
 
-const Mainlogo = styled.img`
-  width: 10rem;
-  height: 13rem;
-  padding: 1.2rem;
-`;
-
-const SocialImg = styled.img`
-  width: 3.5rem;
-  height: 3.5rem;
-  border-radius: 3rem;
-`;
-const SocialWrap = styled.div`
-  flex-direction: row;
-`
-const LoginImage = styled.image`
-  width: 30px;
-  height: 30px;
-`
 function App() {
-  const [message, setMassege] = useState([]);
-
-  useEffect(()=>{
-    fetch("/api/v1/welcome")
-      .then((response)=>{
-        return response.json();
-      })
-      .then((data)=>{
-        setMassege(data);
-      });
-  },[]);
-
-  return (
+   return (
     <>
-      <MainDiv>
-        <Mainlogo src={NOTI} />
-        <h1>{message}</h1>
-        <SocialWrap>
-          <SocialImg style={{ margin: '1rem' }} src={GOOGLE} />
-          <SocialImg style={{ margin: '1rem' }} src={NAVER} />
-          <SocialImg style={{ margin: '1rem' }} src={KAKAO} />
-        </SocialWrap>
-      </MainDiv>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Login/>} />
+          <Route path='/Main' element={<Main/>} />
+        </Routes>
+      </Router>
     </>
   );
 }
---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,74 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable prettier/prettier */
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import styled from "styled-components";
+import NOTI from '../asset/KakaoTalk_20240105_025742662.png';
+import NAVER from '../asset/NAVER.png';
+import GOOGLE from '../asset/GOOGLE.png';
+import KAKAO from '../asset/KAKAO.png';
+
+const MainDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  justify-content: center;
+  background-color: #F9F9F9;
+  height: 100vh;
+  display: flex;
+`;
+
+const Mainlogo = styled.img`
+  width: 10rem;
+  height: 13rem;
+  padding: 1.2rem;
+`;
+
+const SocialImg = styled.img`
+  width: 3.5rem;
+  height: 3.5rem;
+  border-radius: 3rem;
+`;
+const SocialWrap = styled.div`
+  flex-direction: row;
+`
+const LoginImage = styled.image`
+  width: 30px;
+  height: 30px;
+`
+function Login() {
+  const [message, setMassege] = useState([]);
+
+  useEffect(()=>{
+    fetch("/api/v1/welcome")
+      .then((response)=>{
+        return response.json();
+      })
+      .then((data)=>{
+        setMassege(data);
+      });
+  },[]);
+
+  return (
+    <>
+      <MainDiv>
+        <Mainlogo src={NOTI} />
+        <h1>{message}</h1>
+        <SocialWrap>
+          <a href="/Main">
+            <SocialImg style={{ margin: '1rem' }} src={GOOGLE} />
+          </a>
+          <a href="/Main">
+          <SocialImg style={{ margin: '1rem' }} src={NAVER} />
+          </a>
+          <a href="/Main">
+          <SocialImg style={{ margin: '1rem' }} src={KAKAO} />
+          </a>
+        </SocialWrap>
+      </MainDiv>
+    </>
+  );
+}
+
+export default Login;
