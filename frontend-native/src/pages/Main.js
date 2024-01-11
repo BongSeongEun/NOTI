38231041d86789b3eb2ee8c803a32cdb2bf02ec6
seoyoun8694 <102515499+seoyoun8694@users.noreply.github.com>
2024-01-11 17:23:24 +0900

    초기화 + install

---
+++ frontend-native/src/pages/Main.js
@@ -0,0 +1,7 @@
+/* install 명령어
+
+npm install @react-navigation/native --save --legacy-peer-deps
+expo install react-native-gesture-handler react-native-reanimated react-native-screens react-native-safe-area-context @react-native-community/masked-view
+npm install @react-navigation/stack --save --legacy-peer-deps
+
+*/
