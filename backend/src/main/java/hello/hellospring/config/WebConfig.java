576508f311d6e7c04bf408041360aa89da658602
SeongEun <bongfam2@naver.com>
2024-01-15 03:13:45 +0900

    WebConfig 위지 수정

--- backend/src/main/java/hello/hellospring/config/WebConfig.java
+++ backend/src/main/java/hello/hellospring/config/WebConfig.java
@@ -1,4 +1,4 @@
-package hello.hellospring;
+package hello.hellospring.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
