48beba87fb6fbd2f1a27b1591b5f2da0556b32af
seoyoun8694 <102515499+seoyoun8694@users.noreply.github.com>
2024-01-15 18:57:52 +0900

    로그인 성공페이지까지~

---
+++ frontend-native/src/pages/Register_Success.js
@@ -0,0 +1,118 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable react/self-closing-comp */
+/* eslint-disable no-trailing-spaces */
+/* eslint-disable no-mixed-spaces-and-tabs */
+
+import styled from "styled-components/native"
+
+import React from 'react';
+import {
+	View,
+	Text,
+	Button,
+	Image,
+	TextInput,
+	TouchableOpacity,
+	ScrollView,
+	Keyboard,
+} from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import 'react-native-gesture-handler';
+
+import profile_g from "../asset/profile_g.png";
+import deco from "../asset/deco.png";
+
+function Register_Success({ }) {
+	const navigation = useNavigation();
+	const name = "홍길동"
+
+	return (
+		<ScrollView>
+			<MainViewStyle>
+				<RegularText>
+					프로필 생성 완료!
+				</RegularText>
+
+				<MainText>
+					{name} 님! 노티에 {'\n'} 오신것을 환영해요
+				</MainText>
+
+				<ProfileContainer>
+					<Profile source={profile_g}></Profile>
+					<Deco source={deco}></Deco>
+				</ProfileContainer>
+
+				<ResultButton onPress={() => navigation.navigate("Todo")}>
+					<ResultText>완료</ResultText>
+				</ResultButton>
+
+			</MainViewStyle>
+		</ScrollView>
+	);
+}
+
+const MainViewStyle = styled.View`
+	flex: 1;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+  	background-color: #333333;
+`;
+
+const Profile = styled.Image`
+  	width: 130px;
+  	height: 130px;
+	margin-top: 50px;
+`;
+
+const Deco = styled.Image`
+  	width: 180px;
+  	height: 150px;
+  	position: absolute;
+  	margin-top: 45px;
+	margin-left: -40px;
+`;
+
+const ProfileContainer = styled.View`
+  	position: relative;
+	display: flex;
+`;
+
+const MainText = styled.Text`
+	color: white;
+	font-size: 20px;
+	font-weight: bold;
+	margin: 20px;
+	text-align: center;
+`;
+
+const RegularText = styled.Text`
+	color: #FF7154;
+	font-size: 15px;
+	font-weight: bold;
+	margin-top: 50px;
+`;
+
+const ResultText = styled.Text`
+	color: white;
+    font-size: 10px;
+	font-weight: bold;
+    font-family: Pretendard;
+	text-align: center;
+	margin: 13px;
+`;
+
+const ResultButton = styled.TouchableOpacity`
+	width: 300px;
+	height: 40px;
+	background-color: #FF7154;
+	border-radius: 15px;
+	margin: 50px;
+	margin-bottom: 80px;
+	margin-top: 137px;
+	justify-content: center;
+  	align-items: center;
+	display: flex;
+`; 
+
+export default Register_Success;
