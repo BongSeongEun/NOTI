d3027f7c168f4d86f7eb7c8b145df329a4cc5888
SeongEun <bongfam2@naver.com>
2024-01-17 19:43:45 +0900

    gpt 채팅구현

--- frontend-native/App.js
+++ frontend-native/App.js
@@ -1,69 +1,3 @@
-/* eslint-disable quotes */
-/* eslint-disable prettier/prettier */
-///* eslint-disable prettier/prettier */
-//
-//import React, {useState, useEffect} from 'react';
-//import {
-//  SafeAreaView,
-//  ScrollView,
-//  StatusBar,
-//  StyleSheet,
-//  useColorScheme,
-//  View,
-//} from 'react-native';
-//import styled from 'styled-components/native';
-//import NOTI from './asset/KakaoTalk_20240105_025742662.png';
-//import NAVER from './asset/NAVER.png';
-//import GOOGLE from './asset/GOOGLE.png';
-//import KAKAO from './asset/KAKAO.png';
-//
-//const Container = styled.View`
-//  flex: 1;
-//  background-color: '#f9f9f9';
-//  align-items: center;
-//  justify-content: center;
-//`;
-//const Text = styled.Text`
-//  font-size: 22px;
-//  color: black;
-//  margin-bottom: 20px;
-//`;
-//const Image = styled.Image`
-//  width: 60%;
-//  height: 40%;
-//  margin-bottom: 20px;
-//`;
-//const LoginImage = styled.Image`
-//  width: 85%;
-//  height: 7%;
-//  margin-bottom: 20px;
-//`;
-//
-//function App() {
-//  const [message, setMessage] = useState([]);
-//
-//  useEffect(() => {
-//    fetch('http://localhost:4000/api/v1/welcome')
-//      .then(response => response.json())
-//      .then(data => setMessage(data))
-//      .catch(error => {
-//        console.log(error);
-//      });
-//  }, []);
-//
-//  return (
-//    <Container>
-//      <Image source={NOTI} />
-//      <Text>{message}</Text>
-//      <LoginImage resizeMode={'contain'} source={NAVER} />
-//      <LoginImage resizeMode={'contain'} source={GOOGLE} />
-//      <LoginImage resizeMode={'contain'} source={KAKAO} />
-//    </Container>
-//  );
-//}
-//
-//export default App;
-
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
@@ -77,6 +11,7 @@ import Register from "./src/pages/Register";
 import Register_Success from "./src/pages/Register_Success";
 import Setting from "./src/pages/Setting";
 import Todo from "./src/pages/Todo";
+import Chatting from "./src/pages/Chatting";
 
 const Stack = createStackNavigator();
 
@@ -90,6 +25,8 @@ export default function App() {
 			<Stack.Screen name="Register_Success" component={Register_Success} />
 		
 			<Stack.Screen name="Todo" component={Todo} />
+
+			<Stack.Screen name="Chatting" component={Chatting} />
       	</Stack.Navigator>
     </NavigationContainer>
   );
--- frontend-native/metro.config.js
+++ frontend-native/metro.config.js
@@ -6,12 +6,17 @@
  */
 
 module.exports = {
+  resolver: {
+    /* resolver options */
+    sourceExts: ['jsx', 'js', 'ts', 'tsx'],
+  },
+  maxWorkers: 2,
   transformer: {
     getTransformOptions: async () => ({
       transform: {
         experimentalImportSupport: false,
-        inlineRequires: true,
+        inlineRequires: false,
       },
     }),
   },
-};
+}
