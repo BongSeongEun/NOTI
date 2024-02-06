d9eab7e5677b158b38f4a681fc872cbfecfe4260
SeongEun <bongfam2@naver.com>
2024-02-06 17:04:13 +0900

    서윤이가 남기고간 그것

--- frontend-native/App.js
+++ frontend-native/App.js
@@ -1,47 +1,42 @@
-import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-import styled from "styled-components/native";
+/* eslint-disable prettier/prettier */
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import styled from 'styled-components/native';
 import 'react-native-gesture-handler';
 
-import Login from "./src/pages/Login";
-import Coop from "./src/pages/Coop";
-import Coop_Main from "./src/pages/Coop_Main";
-import Diary from "./src/pages/Diary";
-import Register from "./src/pages/Register";
-import Register_Success from "./src/pages/Register_Success";
-import Setting from "./src/pages/Setting";
-import Todo from "./src/pages/Todo";
-<<<<<<< HEAD
-import Chatting from "./src/pages/Chatting";
-=======
-import Todo_Add from "./src/pages/Todo_Add";
->>>>>>> 181fc5c5f7ba9b4b71c13e44dd04dbb1d41b2b53
+import Login from './src/pages/Login';
+import Coop from './src/pages/Coop';
+import Coop_Main from './src/pages/Coop_Main';
+import Diary from './src/pages/Diary';
+import Register from './src/pages/Register';
+import Register_Success from './src/pages/Register_Success';
+import Setting from './src/pages/Setting';
+import Todo from './src/pages/Todo';
+import Chatting from './src/pages/Chatting';
+import Todo_Add from './src/pages/Todo_Add';
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-    	<Stack.Navigator initialRouteName="Login">
-        	<Stack.Screen name="Login" component={Login} />
-        
-			<Stack.Screen name="Register" component={Register} />
-			  <Stack.Screen name="Register_Success" component={Register_Success} />
-		
-			<Stack.Screen name="Todo" component={Todo} />
-<<<<<<< HEAD
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={Login} />
 
-			<Stack.Screen name="Chatting" component={Chatting} />
-=======
-			<Stack.Screen name="Todo_Add" component={Todo_Add} />
-			  <Stack.Screen name="Coop" component={Coop} />
-			  <Stack.Screen name="Coop_Main" component={Coop_Main} />
+        <Stack.Screen name="Register" component={Register} />
+        <Stack.Screen name="Register_Success" component={Register_Success} />
 
-			<Stack.Screen name="Diary" component={Diary} />
-			<Stack.Screen name="Setting" component={Setting} />
->>>>>>> 181fc5c5f7ba9b4b71c13e44dd04dbb1d41b2b53
-      	</Stack.Navigator>
+        <Stack.Screen name="Todo" component={Todo} />
+
+        <Stack.Screen name="Chatting" component={Chatting} />
+        <Stack.Screen name="Todo_Add" component={Todo_Add} />
+        <Stack.Screen name="Coop" component={Coop} />
+        <Stack.Screen name="Coop_Main" component={Coop_Main} />
+
+        <Stack.Screen name="Diary" component={Diary} />
+        <Stack.Screen name="Setting" component={Setting} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
