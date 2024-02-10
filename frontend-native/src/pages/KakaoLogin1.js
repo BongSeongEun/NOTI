91b87fec7f7ff549f666f6e6e4df3cdc755414c2
SeongEun <bongfam2@naver.com>
2024-02-11 03:45:30 +0900

    블로그 쓸러 갈게요

--- frontend-native/src/pages/KakaoLogin1.js
+++ frontend-native/src/pages/KakaoLogin1.js
@@ -9,6 +9,16 @@ import {Dimensions} from 'react-native';
 // 디바이스의 화면 너비와 높이를 가져옵니다.
 
 const KakaoLogin = () => {
+  const navigation = useNavigation();
+
+  const handleNavigationStateChange = navState => {
+    // 성공 URL 패턴 확인 예시: http://192.168.75.126:4000/authnative?success
+    if (navState.url.includes('success')) {
+      // 조건 충족 시 'Register' 화면으로 네비게이션
+      navigation.navigate('Register');
+    }
+  };
+
   return (
     <SafeAreaView style={{flex: 1}}>
       <WebView
@@ -16,6 +26,7 @@ const KakaoLogin = () => {
         source={{
           uri: 'https://kauth.kakao.com/oauth/authorize?client_id=77cf97c36317f2622a926b9ddb30f96f&redirect_uri=http://192.168.75.126:4000/authnative&response_type=code',
         }}
+        onNavigationStateChange={handleNavigationStateChange}
       />
     </SafeAreaView>
   );
