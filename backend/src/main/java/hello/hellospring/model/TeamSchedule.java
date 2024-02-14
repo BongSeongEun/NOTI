a4704dbb8878e4d989d83cf121eaa3c087392dc6
SeongEun <bongfam2@naver.com>
2024-02-14 13:07:45 +0900

    모델 생성

--- backend/src/main/java/hello/hellospring/model/TeamSchedule.java
+++ backend/src/main/java/hello/hellospring/model/TeamSchedule.java
@@ -1,6 +1,18 @@
 package hello.hellospring.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "team_schedule")
 public class TeamSchedule {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "team_schedule_id")
+    private Long teamScheduleId;
 
     
 
