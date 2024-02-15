fc2872044c657a605fa6fd8f7ba30a50ef1aee15
parkhss <115133766+parkhss@users.noreply.github.com>
2024-02-15 18:50:07 +0900

    GPT 대화내역 리스트 조회 API 구현

--- backend/src/main/java/hello/hellospring/controller/GptController.java
+++ backend/src/main/java/hello/hellospring/controller/GptController.java
@@ -5,11 +5,9 @@ import hello.hellospring.model.Chat;
 import hello.hellospring.repository.ChatRepository;
 import hello.hellospring.service.GptService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -53,6 +51,11 @@ public class GptController {
             return "GPT API 호출 오류가 발생했습니다.";
         }
 
+    }
+
+    @GetMapping("/api/v3/chatlist/{userId}") // 채팅내역 가져오기
+    public List<Chat> getChatListByUserId(@PathVariable Long userId) {
+        return chatRepository.findByUserId(userId);
     }
 
 }
--- backend/src/main/java/hello/hellospring/repository/ChatRepository.java
+++ backend/src/main/java/hello/hellospring/repository/ChatRepository.java
@@ -3,6 +3,9 @@ package hello.hellospring.repository;
 import hello.hellospring.model.Chat;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ChatRepository extends JpaRepository<Chat, Long> {
 
+    List<Chat> findByUserId(Long userId);
 }
