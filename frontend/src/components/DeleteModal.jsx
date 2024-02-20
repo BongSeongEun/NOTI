8b81a8a7f4d956ec75435c0375106c157601b843
jhyeonn <102514763+jhyeonn@users.noreply.github.com>
2024-02-20 13:32:36 +0900

    이제는 진짜 끝.

--- frontend/src/components/DeleteModal.jsx
+++ frontend/src/components/DeleteModal.jsx
@@ -8,7 +8,7 @@ const ModalBackdrop = styled.div`
   left: 0;
   width: 100%;
   height: 100%;
-  background-color: rgba(0, 0, 0, 0.5);
+  background-color: rgba(0, 0, 0, 0.1);
   display: flex;
   justify-content: center;
   align-items: center;
