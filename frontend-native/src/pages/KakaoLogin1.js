39e048b9326cdc69b26cfef0e094beb4a8520358
SeongEun <bongfam2@naver.com>
2024-03-14 01:15:58 +0900

    네이티브 아이피 설정

--- frontend-native/src/pages/KakaoLogin1.js
+++ frontend-native/src/pages/KakaoLogin1.js
@@ -6,33 +6,33 @@ import {SafeAreaView} from 'react-native-safe-area-context';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const KakaoLogin = () => {
-	const navigation = useNavigation();
-	const route = useRoute();
-	const { nextScreen } = route.params;
+  const navigation = useNavigation();
+  const route = useRoute();
+  const {nextScreen} = route.params;
 
-	const handleNavigationStateChange = navState => {
-		if (navState.url.includes('success')) {
-			const { url } = navState;
-			const jwtToken = url.split('token=')[1];
-			AsyncStorage.setItem('token', jwtToken)
-				.then(() => {
-					navigation.navigate(nextScreen);
-				})
-				.catch(error => console.error('Error storing token:', error));
-		}
-	};
+  const handleNavigationStateChange = navState => {
+    if (navState.url.includes('success')) {
+      const {url} = navState;
+      const jwtToken = url.split('token=')[1];
+      AsyncStorage.setItem('token', jwtToken)
+        .then(() => {
+          navigation.navigate(nextScreen);
+        })
+        .catch(error => console.error('Error storing token:', error));
+    }
+  };
 
-	return (
-		<SafeAreaView style={{ flex: 1 }}>
-			<WebView
-				style={{ flex: 1 }}
-				source={{
-					uri: 'https://kauth.kakao.com/oauth/authorize?client_id=77cf97c36317f2622a926b9ddb30f96f&redirect_uri=http://192.168.30.214:4000/authnative&response_type=code',
-				}}
-				onNavigationStateChange={handleNavigationStateChange}
-			/>
-		</SafeAreaView>
-	);
+  return (
+    <SafeAreaView style={{flex: 1}}>
+      <WebView
+        style={{flex: 1}}
+        source={{
+          uri: 'https://kauth.kakao.com/oauth/authorize?client_id=77cf97c36317f2622a926b9ddb30f96f&redirect_uri=http://15.164.151.130:4000/authnative&response_type=code',
+        }}
+        onNavigationStateChange={handleNavigationStateChange}
+      />
+    </SafeAreaView>
+  );
 };
 
 export default KakaoLogin;
