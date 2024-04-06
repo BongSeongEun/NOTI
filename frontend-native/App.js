5fa3771a72d803b9a6bded0e50f8dffdc2e4d87a
SeongEun <bongfam2@naver.com>
2024-04-06 20:16:45 +0900

    fix: native error fix

--- frontend-native/App.js
+++ frontend-native/App.js
@@ -1,59 +1,52 @@
 /* eslint-disable prettier/prettier */
-import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-import styled from "styled-components/native";
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import styled from 'styled-components/native';
 import 'react-native-gesture-handler';
 
-import Login from "./src/pages/Login";
-import Coop from "./src/pages/Coop";
-import Coop_Main from "./src/pages/Coop_Main";
-import Diary_Main from "./src/pages/Diary_Main";
-import Diary from "./src/pages/Diary";
-import Register from "./src/pages/Register";
-import Register_Success from "./src/pages/Register_Success";
-import Setting from "./src/pages/Setting";
-import Todo from "./src/pages/Todo";
-import Chatting from "./src/pages/Chatting";
-import Todo_Add from "./src/pages/Todo_Add";
+import Login from './src/pages/Login';
+import Coop from './src/pages/Coop';
+import Coop_Main from './src/pages/Coop_Main';
+import Diary_Main from './src/pages/Diary_Main';
+import Diary from './src/pages/Diary';
+import Register from './src/pages/Register';
+import Register_Success from './src/pages/Register_Success';
+import Setting from './src/pages/Setting';
+import Todo from './src/pages/Todo';
+import Chatting from './src/pages/Chatting';
+import Todo_Add from './src/pages/Todo_Add';
 import KakaoLogin from './src/pages/KakaoLogin1';
 import Redirection from './src/pages/Redirection';
-import Stat from "./src/pages/Stat";
+import Stat from './src/pages/Stat';
 
 const Stack = createStackNavigator();
-
-const linking = {
-  prefixes: ['http://192.168.103.245:4000/authnative'], // 여기에 딥 링크에 사용할 스킴을 추가합니다.
-  config: {
-    screens: {
-      Redirection: 'Redirection', // 여기에 리디렉션 URL의 경로와 매칭되는 화면 이름을 설정합니다.
-    },
-  },
-};
 
 export default function App() {
   return (
     <NavigationContainer>
-    	<Stack.Navigator initialRouteName="Login" screenOptions={{ headerShown: false }}>
-        	<Stack.Screen name="Login" component={Login} />
-          <Stack.Screen name="kakaoLogin" component={KakaoLogin} />
-          <Stack.Screen name="Redirection" component={Redirection} />  
-        
-			<Stack.Screen name="Register" component={Register} />
-			  <Stack.Screen name="Register_Success" component={Register_Success} />
-		
-			<Stack.Screen name="Todo" component={Todo} />
+      <Stack.Navigator
+        initialRouteName="Login"
+        screenOptions={{headerShown: false}}>
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="kakaoLogin" component={KakaoLogin} />
+        <Stack.Screen name="Redirection" component={Redirection} />
 
-			<Stack.Screen name="Chatting" component={Chatting} />
-			<Stack.Screen name="Todo_Add" component={Todo_Add} />
-			  <Stack.Screen name="Coop" component={Coop} />
-			  <Stack.Screen name="Coop_Main" component={Coop_Main} />
+        <Stack.Screen name="Register" component={Register} />
+        <Stack.Screen name="Register_Success" component={Register_Success} />
 
-			  <Stack.Screen name="Diary_Main" component={Diary_Main} />
-			  <Stack.Screen name="Diary" component={Diary} />
-			  <Stack.Screen name="Setting" component={Setting} />
-			  <Stack.Screen name="Stat" component={Stat} />
-      	</Stack.Navigator>
+        <Stack.Screen name="Todo" component={Todo} />
+
+        <Stack.Screen name="Chatting" component={Chatting} />
+        <Stack.Screen name="Todo_Add" component={Todo_Add} />
+        <Stack.Screen name="Coop" component={Coop} />
+        <Stack.Screen name="Coop_Main" component={Coop_Main} />
+
+        <Stack.Screen name="Diary_Main" component={Diary_Main} />
+        <Stack.Screen name="Diary" component={Diary} />
+        <Stack.Screen name="Setting" component={Setting} />
+        <Stack.Screen name="Stat" component={Stat} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
--- frontend-native/src/pages/Stat.js
+++ frontend-native/src/pages/Stat.js
@@ -3,173 +3,172 @@
 /* eslint-disable no-trailing-spaces */
 /* eslint-disable no-mixed-spaces-and-tabs */
 
-import styled, {ThemeProvider} from "styled-components/native"
-import React, { useState, useEffect } from 'react';
+import styled, {ThemeProvider} from 'styled-components/native';
+import React, {useState, useEffect} from 'react';
 import {
-	ScrollView,
-	Text,
-	Modal,
-	Alert,
-	TextInput,
-	Button,
-	TouchableOpacity,
-} from "react-native";
-import { useRoute, useNavigation } from "@react-navigation/native";
+  ScrollView,
+  Text,
+  Modal,
+  Alert,
+  TextInput,
+  Button,
+  TouchableOpacity,
+} from 'react-native';
+import {useRoute, useNavigation} from '@react-navigation/native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import 'react-native-gesture-handler';
-import { decode } from 'base-64';
+import {decode} from 'base-64';
 import axios from 'axios';
-import { PieChart } from "react-native-gifted-charts";
+import {PieChart} from 'react-native-gifted-charts';
 
-import images from "../components/images";
-import Navigation_Bar from "../components/Navigation_Bar";
-import { theme } from '../components/theme';
+import images from '../components/images';
+import Navigation_Bar from '../components/Navigation_Bar';
+import {theme} from '../components/theme';
 
-function Stat({ }) {
-	const navigation = useNavigation();
+function Stat({}) {
+  const navigation = useNavigation();
 
-	const [currentTheme, setCurrentTheme] = useState(theme.OrangeTheme);
-    const [base64Image, setBase64Image] = useState('');
-	const [userNickname, setUserNickname] = useState('');
-	
-	useEffect(() => {
-		const fetchUserData = async () => {
-			const token = await AsyncStorage.getItem('token');
-  
-			if (token) {
-				const userId = getUserIdFromToken(token);
-				try {
-					const response = await axios.get(`http://15.164.151.130:4000/api/v1/userInfo/${userId}`, {
-						headers: {
-							'Authorization': `Bearer ${token}`,
-						},
-					});
-					const userThemeName = response.data.userColor || 'OrangeTheme';
-					const userProfileImage = response.data.userProfile;
-					const nickname = response.data.userNickname;
-  
-					if (theme[userThemeName]) {
-						setCurrentTheme(theme[userThemeName]);
-					}
-					setBase64Image(userProfileImage || '');
-					setUserNickname(nickname || '');
-				} catch (error) {
-					console.error("Error fetching user data:", error);
-				}
-			}
-		};
-		fetchUserData();
-	}, []);
-	
-	const getUserIdFromToken = (token) => {
-		try {
-			const payload = token.split('.')[1];
-			const base64 = payload.replace(/-/g, '+').replace(/_/g, '/');
-			const decodedPayload = decode(base64);
-			const decodedJSON = JSON.parse(decodedPayload);
+  const [currentTheme, setCurrentTheme] = useState(theme.OrangeTheme);
+  const [base64Image, setBase64Image] = useState('');
+  const [userNickname, setUserNickname] = useState('');
 
-			return decodedJSON.id.toString();
-		} catch (error) {
-			console.error('Error decoding token:', error);
-			return null;
-		}
-	};
+  useEffect(() => {
+    const fetchUserData = async () => {
+      const token = await AsyncStorage.getItem('token');
 
-	const pieData = [
-		{ id: 'data1', value: 50, color: currentTheme.color1 },
-		{ id: 'data2', value: 40, color: currentTheme.color2 },
-		{ id: 'data3', value: 20, color: currentTheme.color3 },
-		{ id: 'data4', value: 25, color: currentTheme.color4 },
-		{ id: 'data5', value: 15, color: currentTheme.color5 },
-	];
+      if (token) {
+        const userId = getUserIdFromToken(token);
+        try {
+          const response = await axios.get(
+            `http://15.164.151.130:4000/api/v1/userInfo/${userId}`,
+            {
+              headers: {
+                Authorization: `Bearer ${token}`,
+              },
+            },
+          );
+          const userThemeName = response.data.userColor || 'OrangeTheme';
+          const userProfileImage = response.data.userProfile;
+          const nickname = response.data.userNickname;
 
-	return (
-		<ThemeProvider theme={currentTheme}>
-			<FullView>
-				<ScrollView>
-					<MainView>
-						<MainText>{userNickname} 님의 한 달</MainText>
-						<MainText>노티 활동을 모아봤어요!</MainText>
+          if (theme[userThemeName]) {
+            setCurrentTheme(theme[userThemeName]);
+          }
+          setBase64Image(userProfileImage || '');
+          setUserNickname(nickname || '');
+        } catch (error) {
+          console.error('Error fetching user data:', error);
+        }
+      }
+    };
+    fetchUserData();
+  }, []);
 
-						<MainText>기간 선택</MainText>
+  const getUserIdFromToken = token => {
+    try {
+      const payload = token.split('.')[1];
+      const base64 = payload.replace(/-/g, '+').replace(/_/g, '/');
+      const decodedPayload = decode(base64);
+      const decodedJSON = JSON.parse(decodedPayload);
 
-						<ScrollView
-							horizontal={true}
-							showsHorizontalScrollIndicator={true}
-						>
-							<States color={currentTheme.color1}>
-								<MainText>통계1</MainText>
-							</States>
+      return decodedJSON.id.toString();
+    } catch (error) {
+      console.error('Error decoding token:', error);
+      return null;
+    }
+  };
 
-							<States color={currentTheme.color2}>
-								<MainText>통계2</MainText>
-							</States>
-						</ScrollView>
-					
-						<MainText>상세 리포트</MainText>
+  const pieData = [
+    {id: 'data1', value: 50, color: currentTheme.color1},
+    {id: 'data2', value: 40, color: currentTheme.color2},
+    {id: 'data3', value: 20, color: currentTheme.color3},
+    {id: 'data4', value: 25, color: currentTheme.color4},
+    {id: 'data5', value: 15, color: currentTheme.color5},
+  ];
 
-						<StatFrame>
-							<MainText>통계자료</MainText>
-							<PieChart
-								data={pieData}
-								showText
-								textColor="black"
-								radius={80}
-								textSize={10}
-								focusOnPress
-								showValuesAsLabels
-							/>
-						</StatFrame>
+  return (
+    <ThemeProvider theme={currentTheme}>
+      <FullView>
+        <ScrollView>
+          <MainView>
+            <MainText>{userNickname} 님의 한 달</MainText>
+            <MainText>노티 활동을 모아봤어요!</MainText>
 
-						<StatFrame>
-							<MainText>통계자료</MainText>
-						</StatFrame>
+            <MainText>기간 선택</MainText>
 
-					</MainView>
-				</ScrollView>
-			</FullView>
-		</ThemeProvider>
-	);
+            <ScrollView horizontal={true} showsHorizontalScrollIndicator={true}>
+              <States color={currentTheme.color1}>
+                <MainText>통계1</MainText>
+              </States>
+
+              <States color={currentTheme.color2}>
+                <MainText>통계2</MainText>
+              </States>
+            </ScrollView>
+
+            <MainText>상세 리포트</MainText>
+
+            <StatFrame>
+              <MainText>통계자료</MainText>
+              <PieChart
+                data={pieData}
+                showText
+                textColor="black"
+                radius={80}
+                textSize={10}
+                focusOnPress
+                showValuesAsLabels
+              />
+            </StatFrame>
+
+            <StatFrame>
+              <MainText>통계자료</MainText>
+            </StatFrame>
+          </MainView>
+        </ScrollView>
+      </FullView>
+    </ThemeProvider>
+  );
 }
 
 const FullView = styled.View`
-   width: 100%;
-   background-color: white;
+  width: 100%;
+  background-color: white;
 `;
 
 const MainView = styled(FullView)`
-   height: auto;
-   align-self: center;
-   width: 300px;
+  height: auto;
+  align-self: center;
+  width: 300px;
 `;
 
 const HorisontalView = styled(MainView)`
-   flex-direction: row;
+  flex-direction: row;
 `;
 
 const MainText = styled.Text`
-    font-size: ${props => props.fontSize || "12px"};
-    font-weight: bold;
-    color: ${props => props.color || "black"};
-    text-align: left;
+  font-size: ${props => props.fontSize || '12px'};
+  font-weight: bold;
+  color: ${props => props.color || 'black'};
+  text-align: left;
 `;
 
 const States = styled.TouchableOpacity`
-	width: 290px;
-	height: 100px;
-	border-radius: 15px;
-	background-color: ${props => props.color || "#FF7154"};
-	flex-direction: row;
-	align-items: center;
-	margin: 5px;
+  width: 290px;
+  height: 100px;
+  border-radius: 15px;
+  background-color: ${props => props.color || '#FF7154'};
+  flex-direction: row;
+  align-items: center;
+  margin: 5px;
 `;
 
 const StatFrame = styled.TouchableOpacity`
-	width: 300px;
-	height: 200px;
-	border-radius: 15px;
-	border-width: 1px;
-	border-color: #B7BABF;
+  width: 300px;
+  height: 200px;
+  border-radius: 15px;
+  border-width: 1px;
+  border-color: #b7babf;
 `;
 
 export default Stat;
